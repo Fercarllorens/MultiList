3d53983eb05af8a3855806f811c19ac4a8b7e1dc
FranchoPlz <ferriolsfran@gmail.com>
2021-10-07 20:14:54 +0200

    Moved again

--- Backend/MDLserver/SpotifyAPI/credentials.py
+++
@@ -1,4 +0,0 @@
-#TODO: move to env variables
-CLIENT_ID = "d422ec0e5f294702b63d84a1e060e4d9"
-CLIENT_SECRET = "76b48a883bcc490ca8e4dc504490a3a0"
-REDIRECT_URI = ""
--- Backend/MDLserver/SpotifyAPI/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AuthURL
-
-urlpatterns = [
-    path("get-auth-url", AuthURL.as_view())
-    path("redirect", AuthURL.spotify_callback)
-]
