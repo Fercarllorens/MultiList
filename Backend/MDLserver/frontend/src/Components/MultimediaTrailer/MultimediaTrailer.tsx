2e4a4d85087c6409f93ba8f0e224a1f975b2e1c3
Fernando Carceller Llorens <fercarll@upv.edu.es>
2021-10-19 09:45:04 +0200

    css añadido y mejorado

--- Backend/MDLserver/frontend/src/Components/MultimediaTrailer/MultimediaTrailer.tsx
+++ Backend/MDLserver/frontend/src/Components/MultimediaTrailer/MultimediaTrailer.tsx
@@ -10,9 +10,10 @@ const MultimediaTrailer: React.FC<Props> = (props) => {
     const {trailer} = MultimediaTrailerLogic(props)
         return (
         <div className="data-container-trailer">
-            <video>
+            {/*<video>
                 <source src={trailer != null ? trailer : 'NoVideo'} type=""></source>
-            </video>
+            </video>*/}
+            <img src={'https://us.123rf.com/450wm/seetwo/seetwo1907/seetwo190700208/126635447-ning%C3%BAn-signo-vac%C3%ADo-c%C3%ADrculo-tachado-rojo-signo-no-permitido-aislar-sobre-fondo-blanco-ilustraci%C3%B3n-vec.jpg?ver=6'} alt="No disponible" />
         </div>
     )
 }
--- Backend/MDLserver/frontend/src/Components/Pic/Pic.tsx
+++ Backend/MDLserver/frontend/src/Components/Pic/Pic.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Pic.css'
 import PicLogic from './PicLogic';
 
 interface Props{
@@ -10,8 +10,11 @@ const Pic: React.FC<Props> = (props) => {
     const {pic} = PicLogic(props)
 
     return (
-        <img className="image" src={pic != null ? pic : 'ImageNotFound'} alt="No disponible" />
+        <div className="image">
+            <img src={/*pic != null ? pic : 'ImageNotFound'*/ 'https://us.123rf.com/450wm/seetwo/seetwo1907/seetwo190700208/126635447-ning%C3%BAn-signo-vac%C3%ADo-c%C3%ADrculo-tachado-rojo-signo-no-permitido-aislar-sobre-fondo-blanco-ilustraci%C3%B3n-vec.jpg?ver=6'} alt="No disponible" />
+        </div>
     )
+
 }
 
 export default Pic
