7f178417404550a6582fa284d5054348503b2bbb
Sergio <sergiotrujillogarcia29@gmail.com>
2021-10-29 19:43:35 +0200

    Eliminada jerarquización adicional redundante.

--- Backend/MDLserver/frontend/src/Components/MultimediaContent/MultimediaContent.tsx
+++ Backend/MDLserver/frontend/src/Components/MultimediaContent/MultimediaContent.tsx
@@ -2,11 +2,7 @@ import React, { useEffect } from 'react'
 import './MultimediaContent.css'
 import MultimediaContentLogic from './MultimediaContentLogic'
 //componentes
-import MultimediaTopData from './MultimediaTopData/MultimediaTopData'
-import MultimediaTrailer from './MultimediaTrailer/MultimediaTrailer'
-import MultimediaBottomData from './MultimediaBottomData/MultimediaBottomData'
 import AddList from './AddList/AddList'
-import Pic from '../Pic/Pic'
 
 
 interface Props{
@@ -51,8 +47,17 @@ const MultimediaContent: React.FC<Props> = (props) => {
                 <h3 className="title-top-data">{listTop[0]}</h3>
                 <p className="data"><span className="label-data-type" style={{ backgroundColor: listTop[4] != null ? listTop[4] : 'transparent', borderRadius:20}}>{listTop[1]}</span> - {listTop[2]} - {listTop[3]}</p>
             </div>
-            <Pic url={imageUrl} />
-            <MultimediaTrailer trailer={trailerUrl}/>
+            <div className="image">
+                <img src={imageUrl != null ? imageUrl : 'https://us.123rf.com/450wm/seetwo/seetwo1907/seetwo190700208/126635447-ning%C3%BAn-signo-vac%C3%ADo-c%C3%ADrculo-tachado-rojo-signo-no-permitido-aislar-sobre-fondo-blanco-ilustraci%C3%B3n-vec.jpg?ver=6'} alt="No disponible" />
+            </div>
+            <div className="data-container-trailer">
+            {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/IbJFERe9F9w" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            <video>
+                <source src={'https://www.youtube.com/watch?v=d6z5fsEnIx8&ab_channel=YO%2CINTERNETO'} type=""></source>
+            </video>
+            <img src={'https://us.123rf.com/450wm/seetwo/seetwo1907/seetwo190700208/126635447-ning%C3%BAn-signo-vac%C3%ADo-c%C3%ADrculo-tachado-rojo-signo-no-permitido-aislar-sobre-fondo-blanco-ilustraci%C3%B3n-vec.jpg?ver=6'} alt="No disponible" />
+            */}
+            </div>
             <div className="data-container-bottom">
                 <h4 className="bottom-title">MÁS INFORMACIÓN</h4>
                 {listBottom.map((item) => {
