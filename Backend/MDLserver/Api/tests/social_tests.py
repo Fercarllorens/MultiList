47b185b087b7da5b3dd81fe6c9a58b6ce7629da3
Sergio <sergiotrujillogarcia29@gmail.com>
2021-11-23 20:56:05 +0100

    Creado primer test

---
+++ Backend/MDLserver/Api/tests/social_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from django.test import Client
+
+class Social_Content_Link_Test(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('http://127.0.0.1:8000/video/get-by-id?source_id=tt3398228&source=imdb')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the rendered context contains 5 customers.
+        self.assertEqual('https://www.netflix.com/title/80125588' in response.content.con, True)
