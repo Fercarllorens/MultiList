dbaab0ff3280602e4f71f41ec343b75934252e2f
MigelPuig <90631674+MigelPuig@users.noreply.github.com>
2021-11-26 18:24:59 +0100

    tests tiempo 😡

--- Backend/MDLserver/tests.py
+++ Backend/MDLserver/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 import unittest
+import requests
 
 # Create your tests here.
 
@@ -28,3 +29,9 @@ class Social_Series_Content_Link_Test_Error(unittest.TestCase):
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
         self.assertEqual('https://www.netflix.com/title/80125589' in str(response.json()), False)
+
+class Films_Response_Time_Test(unittest.TestCase):
+    def test_details(self):
+        #Check that response time is less than 1 second
+        time = requests.get("http://127.0.0.1:8000/video/get-by-id?source_id=tt3398228&source=imdb").elapsed.total_seconds()
+        self.assertLess(time, 1)
