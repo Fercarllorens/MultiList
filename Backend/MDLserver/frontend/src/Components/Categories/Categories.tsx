fff9a9fd35278f3888b2b83c7c890279ea536942
Asefron29 <61246972+Asefron29@users.noreply.github.com>
2021-12-01 10:04:47 +0100

    Establecida base de frontend de Categories.

---
+++ Backend/MDLserver/frontend/src/Components/Categories/Categories.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import './Categories.css'
+import CategoriesLogic from './CategoriesLogic'
+
+interface Props{
+}
+
+const Categories: React.FC<Props> = (props) => {
+    const {} = CategoriesLogic(props)
+
+    return(
+        <></>
+    )
+}
+
+export default Categories
---
+++ Backend/MDLserver/frontend/src/Components/Categories/CategoriesLogic.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import {useForm} from 'react-hook-form';
+import { useLocation } from 'react-router';
+import { useHistory } from 'react-router-dom';
+import { fetchHandler, fetchHandlerCb } from '../fetchHandler'
+
+interface Props {
+}
+
+const CategoriesLogic = (props:Props) => {
+
+    return {}
+}
+
+export default CategoriesLogic
