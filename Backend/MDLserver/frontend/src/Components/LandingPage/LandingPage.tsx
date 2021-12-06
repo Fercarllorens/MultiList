ec17186bdeed5381d874264d92105f7ee1c20ab1
FranchoPlz <ferriolsfran@gmail.com>
2021-12-06 16:49:10 +0100

    mierdilla de la landing

--- Backend/MDLserver/frontend/src/Components/LandingPage/LandingPage.tsx
+++ Backend/MDLserver/frontend/src/Components/LandingPage/LandingPage.tsx
@@ -5,29 +5,38 @@ function LandingPage(): ReactElement {
     return (
         <div className="landingpage-screen">
             {/* FIRST SCREEN */}
-            <h1 className="landingpage-screen__title">DON'T KNOW WHICH FILM TO WATCH OR SONG TO LISTEN TO?</h1>
-            <h2 className="landingpage-screen__subtitle">LET US HELP YOU OUT!</h2>
-            <button>REGISTER HERE!</button>
-            <h3>THE SOCIAL NETWORK FOR ENTERTAINMENT LOVERS</h3>
+            <div className="landingpage-screen__section" id="register">
+                <div className="landingpage-screen__bg-image"></div>
+                <h1 className="landingpage-screen__title">DON'T KNOW WHICH FILM TO WATCH OR SONG TO LISTEN TO?</h1>
+                <h2 className="landingpage-screen__subtitle">LET US HELP YOU OUT!</h2>
+                <button className="landingpage-screen__register-button">REGISTER HERE!</button>
+                <h3>THE SOCIAL NETWORK FOR ENTERTAINMENT LOVERS</h3>
+            </div>
             {/* SECOND SCREEN */}
-            <h1>SAVE CONTENTS.<br/>REVIEW THEM.<br/>ENJOY.</h1>
-            <p>
-            Sigue a otros usuarios para ver sus listas. <br/>
-            Organiza todos los contenidos a tu gusto. <br/>
-            Consulta dónde los puedes encontrar. <br/>
-            Valóralos y recomiéndale a tus amigos y a todo el mundo lo que a ti te gusta.
-            </p>
-            <img alt="sample selector"/>
-            <img alt="sample content previews"/>
+            <div className="landingpage-screen__section" id="contents">
+                <h1>SAVE CONTENTS.<br />REVIEW THEM.<br />ENJOY.</h1>
+                <p>
+                    Sigue a otros usuarios para ver sus listas. <br />
+                    Organiza todos los contenidos a tu gusto. <br />
+                    Consulta dónde los puedes encontrar. <br />
+                    Valóralos y recomiéndale a tus amigos y a todo el mundo lo que a ti te gusta.
+                </p>
+                <img alt="sample selector" />
+                <img alt="sample content previews" />
+            </div>
             {/* THIRD SCREEN */}
-            <img alt="sample top films"/>
-            <h1>CHECK THE TOP CONTENTS OF ALL TIME</h1>
-            <p>you'll find global, national and weekly tops for any kind of media</p>
-            <img alt="sample top lists"/>
+            <div className="landingpage-screen__section" id="tops">
+                <img alt="sample top films" />
+                <h1>CHECK THE TOP CONTENTS OF ALL TIME</h1>
+                <p>you'll find global, national and weekly tops for any kind of media</p>
+                <img alt="sample top lists" />
+            </div>
             {/* FOURTH SCREEN */}
-            <h1>DO YOU WANT TO BECOME A FAMOUS CRITIC?</h1>
-            <p>The more content you value and the more interactions your opinions have, the more chances you have of reaching other users and that they follow your recommendations.</p>
-            <img alt="sample user profile"/>
+            <div className="landingpage-screen__section" id="profiles">
+                <h1>DO YOU WANT TO BECOME A FAMOUS CRITIC?</h1>
+                <p>The more content you value and the more interactions your opinions have, the more chances you have of reaching other users and that they follow your recommendations.</p>
+                <img alt="sample user profile" />
+            </div>
         </div>
     )
 }
--- Backend/MDLserver/frontend/src/Components/Register/Register.tsx
+++ Backend/MDLserver/frontend/src/Components/Register/Register.tsx
@@ -1,40 +1,44 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link, RouteComponentProps } from "react-router-dom";
 import RegisterLogic from './RegisterLogic'
 import './Register.css'
 
-const Register: React.FC<RouteComponentProps> = ({history}) => {
-    const {pic, username, email, password, confirmPassword, error, setUsername, setEmail, setPassword, setConfirmPassword, registerHandler, updatePic} = RegisterLogic(history)
+const Register: React.FC<RouteComponentProps> = ({ history }) => {
+    const { pic, username, email, password, confirmPassword, error, setUsername, setEmail, setPassword, setConfirmPassword, registerHandler, updatePic } = RegisterLogic(history)
     const defaultUserPic = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pinclipart.com%2Fpicdir%2Fbig%2F157-1578186_user-profile-default-image-png-clipart.png&f=1&nofb=1'
 
     useEffect(() => {
-        if(localStorage.getItem('authToken')) history.push('/')
+        if (localStorage.getItem('authToken')) history.push('/')
     }, [history])
 
     return (
         <div className='register-screen'>
             <div className='register-screen_bg-image'></div>
             <form className='register-screen__form' onSubmit={registerHandler}>
-                <h3 className='register-screen__title'>Register on Multilist</h3>
-                {error&& <span className='error-message'>{error}</span>}
-                <input type='text' required id='name' placeholder='Username' value={username} className='form-text-input'
-                    onChange={(e) => {setUsername(e.target.value)}}/>
+                <div className='register-screen__side'>
+                    <h3 className='register-screen__title'>Register on Multilist</h3>
+                    {error && <span className='error-message'>{error}</span>}
+                    <input type='text' required id='name' placeholder='Username' value={username} className='form-text-input'
+                        onChange={(e) => { setUsername(e.target.value) }} />
 
-                <input type='email' required id='email' placeholder='Email' value={email} className='form-text-input'
-                    onChange={(e) => {setEmail(e.target.value)}}/>
+                    <input type='email' required id='email' placeholder='Email' value={email} className='form-text-input'
+                        onChange={(e) => { setEmail(e.target.value) }} />
 
-                <input type='password' required id='password' placeholder='Password' value={password} className='form-text-input'
-                    onChange={(e) => {setPassword(e.target.value)}}/>
+                    <input type='password' required id='password' placeholder='Password' value={password} className='form-text-input'
+                        onChange={(e) => { setPassword(e.target.value) }} />
 
-                <input type='password' required id='confirmPassword' placeholder='Confirm Password' value={confirmPassword} className='form-text-input'
-                    onChange={(e) => {setConfirmPassword(e.target.value)}}/>
-                <span className='register-screen__subtext'>Already have an account? <Link to='/login'>Login here!</Link></span>
-                <div className='pic-group'>
-                    <img src={pic? pic : defaultUserPic} alt="Selected user image" />
-                    <input type='file'  accept=".jpg, .png, .jpeg" id='Pic' className="form-file-input"
-                    onChange={updatePic}/>
+                    <input type='password' required id='confirmPassword' placeholder='Confirm Password' value={confirmPassword} className='form-text-input'
+                        onChange={(e) => { setConfirmPassword(e.target.value) }} />
+                    <span className='register-screen__subtext'>Already have an account? <Link to='/login'>Login here!</Link></span>
                 </div>
-                <input type='submit' className='form-btn' value='Register'/>
+                <div className='register-screen__side'>
+                    <div className='pic-group'>
+                        <img src={pic ? pic : defaultUserPic} alt="Selected user image" />
+                        <input type='file' accept=".jpg, .png, .jpeg" id='Pic' className="form-file-input"
+                            onChange={updatePic} />
+                    </div>
+                    <input type='submit' className='form-btn' value='Register' />
+                </div>
             </form>
         </div>
     )
