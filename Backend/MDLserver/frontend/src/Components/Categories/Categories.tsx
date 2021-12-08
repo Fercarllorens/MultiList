bb12144629c3c13ba9d6014a110fb181ae491555
Sergio <sergiotrujillogarcia29@gmail.com>
2021-12-08 19:27:18 +0100

    Ya se añaden y se persiste el añadido en tiempo de ejecución

--- Backend/MDLserver/frontend/src/Components/Categories/Categories.tsx
+++ Backend/MDLserver/frontend/src/Components/Categories/Categories.tsx
@@ -3,14 +3,13 @@ import './Categories.css'
 import CategoriesLogic from './CategoriesLogic'
 import SearchBar from "../Finder/SearchBar/SearchBar"
 import Filters from "../Finder/Filters/Filters"
-import CategoryPreview from './CategoryPreview/CategoryPreview'
 
 interface Props{
 }
 
 const Categories: React.FC<Props> = (props) => {
     const {getFilmCategories, filmsCategories, getSeriesCategories, seriesCategories, getSongsCategories, songsCategories, find, selectType, type_selected,
-        filteredFilmsCategories, filteredSeriesCategories, filteredSongsCategories, getUserAndUserCategories, userCategories} = CategoriesLogic(props)
+        filteredFilmsCategories, filteredSeriesCategories, filteredSongsCategories, getUserAndUserCategories, userCategories, addCategory} = CategoriesLogic(props)
 
     useEffect(() => {
         getUserAndUserCategories()
@@ -28,19 +27,43 @@ const Categories: React.FC<Props> = (props) => {
             <Filters selectType={selectType} type_selected={type_selected}/>         
             {type_selected.films_selected &&
                     filteredFilmsCategories?.map((element) => (
-                        <CategoryPreview category={element}/>
+                        <div>
+                            <div className="CategoryName">
+                                {element.name}
+                            </div>
+                            {userCategories?.includes(element.id) ? 
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Green_tick.svg/1200px-Green_tick.svg.png" width="100" height="100"/> :
+                                <input type="image" onClick={()=>{addCategory(element)}} src="https://static.thenounproject.com/png/2453491-200.png" width="100" height="100"/>
+                            }
+                        </div>
                         )
                     )
             }
             {type_selected.series_selected && 
                 filteredSeriesCategories?.map((element) => (
-                        <CategoryPreview category={element}/>
+                        <div>
+                            <div className="CategoryName">
+                                {element.name}
+                            </div>
+                            {userCategories?.includes(element.id) ? 
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Green_tick.svg/1200px-Green_tick.svg.png" width="100" height="100"/> :
+                                <input type="image" onClick={()=>{addCategory(element)}} src="https://static.thenounproject.com/png/2453491-200.png" width="100" height="100"/>
+                            }
+                        </div>
                         )
                     )
             }
             {type_selected.songs_selected && 
                 filteredSongsCategories?.map((element) => (
-                        <CategoryPreview category={element}/>
+                        <div>
+                            <div className="CategoryName">
+                                {element.name}
+                            </div>
+                            {userCategories?.includes(element.id) ? 
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Green_tick.svg/1200px-Green_tick.svg.png" width="100" height="100"/> :
+                                <input type="image" onClick={()=>{addCategory(element)}} src="https://static.thenounproject.com/png/2453491-200.png" width="100" height="100"/>
+                            }
+                        </div>
                         )
                     )
             }
