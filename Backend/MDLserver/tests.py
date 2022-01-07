ba744d7adf4329cafcc453d4bd376820dd61f294
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2022-01-07 18:42:41 +0100

    Tests

--- Backend/MDLserver/tests.py
+++ Backend/MDLserver/tests.py
@@ -194,15 +194,6 @@ class User_View_Get_Content_Comments_Test_Empty(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual("[]", str(response.json()))
 
-#Non Functional
-
-class Content_Response_Time_Test(unittest.TestCase):
-    def test_details(self):
-        #Check that response time is less than 1 second
-        time = requests.get("http://127.0.0.1:8000/video/get-show?query=bojack&page=1").elapsed.total_seconds()
-        self.assertLess(time, 1)
-
-
 # Contenido Multimedia
 
 class Content_Request_Get_Film_True(unittest.TestCase):
@@ -261,3 +252,65 @@ class Content_Request_Get_Show_False(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertFalse('61222' in str(response.json()))
         self.assertFalse('BoJack Horseman' in str(response.json()))
+
+class Search_Songs_Test_Correct(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('http://127.0.0.1:8000/spotify/search?query=Despacito&type=track&user=46')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('Despacito' in str(response.json()), True)
+
+class Search_Songs_Test_Error(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('http://127.0.0.1:8000/spotify/search?query=Despacito&type=track&user=46')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('Gangnam Style' in str(response.json()), False)
+
+class Search_Films_Test_Correct(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('http://127.0.0.1:8000/video/get-film?query=Interstellar&page=1')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('Interstellar' in str(response.json()), True)
+
+class Search_Films_Test_Error(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('http://127.0.0.1:8000/video/get-film?query=Interstellar&page=1')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('Inception' in str(response.json()), False)
+
+
+
+#Non Functional
+
+class Content_Response_Time_Test(unittest.TestCase):
+    def test_details(self):
+        #Check that response time is less than 1 second
+        time = requests.get("http://127.0.0.1:8000/video/get-show?query=bojack&page=1").elapsed.total_seconds()
+        self.assertLess(time, 1)
